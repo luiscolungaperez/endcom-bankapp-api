e02c7e9a9b471453c4d15c9aab40ea24057fbf0e
luiscolungaperez <perez.luis.colunga@gmail.com>
2021-01-17 13:53:20 -0600

    [FIX] id autoincrement + number account

--- src/components/user/controller.js
+++ src/components/user/controller.js
@@ -3,16 +3,25 @@ const User = require('./model');
 const { zfill, randomNumber} = require('../../utils/zfill');
 
 const registerUser = ( userData ) => {
-  return new Promise(async (resolve, reject) => {
-    userData.account = randomNumber();
-    //build user Schema
-    const user = new User(userData);
-    // Add new userAccount
-    user.save((error, newUser) => {
-      return error 
-        ? reject('[Controller ERROR ]: On save, ' + error)
-        : resolve(newUser);
-    });
+  return new Promise((resolve, reject) => {
+
+    User.findOne({})
+      .sort({ _id: 'desc' })
+      .then(consult => {
+
+        userData._id = consult._id + 1;
+        userData.account = randomNumber() + zfill(userData._id, 3);
+        
+        //build user Schema
+        const user = new User(userData);
+    
+        // Add new userAccount
+        user.save((error, newUser) => {
+          return error 
+            ? reject('[Controller ERROR ]: On save, ' + error)
+            : resolve(newUser);
+        });
+      });
   });
 };
 
